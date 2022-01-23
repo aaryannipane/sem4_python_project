03aa4e600d53d9633ca840a36150b089697deb25
Aryan Nipane <aaryan28102002@gmail.com>
2022-01-23 20:54:21 +0530

    updated admin page

--- GymManagementSystem/main/admin.py
+++ GymManagementSystem/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display=('alt_text', 'image_tag')
+
+admin.site.register(models.Banners, BannerAdmin)
--- GymManagementSystem/main/models.py
+++ GymManagementSystem/main/models.py
@@ -1,7 +1,22 @@
+from distutils.command.upload import upload
 from turtle import title
 from django.db import models
+from django.utils.html import mark_safe
+
 
 # Create your models here.
+
+class Banners(models.Model):
+    img = models.ImageField(upload_to='banners/')
+    alt_text = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.alt_text
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="80" />' % (self.img.url))
+
+
 class Service(models.Model):
     title = models.CharField(max_length=150)
     detail = models.TextField()
